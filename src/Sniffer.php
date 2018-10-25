5d1ac8d2c1b4243792e466b71304083e026d1c71
Gumkle <dziedzicdev@gmail.com>
2018-10-25 23:12:14 +0200

    First version, added dependencies

---
+++ src/Sniffer.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace Sniffer;
+
+
+use Illuminate\Database\Eloquent\Builder;
+use TypeError;
+
+class Sniffer
+{
+
+    /**
+     * Try to apply search filter and handle any errors
+     *
+     * @param Builder $query
+     * @param $keyword
+     * @param array $columns
+     * @return string
+     */
+    public static function searchFilter(Builder $query, $keyword, Array $columns)
+    {
+        try{
+            self::applySearchFilter($query, $keyword, $columns);
+        } catch (TypeError $e){
+            return 'No keyword';
+        }
+    }
+
+    /**
+     * Search for keyword in given columns and save results to query
+     *
+     * @param Builder $query
+     * @param String $keyword
+     * @param array $columns
+     * @return string
+     */
+    public static function applySearchFilter(Builder $query, String $keyword, Array $columns)
+    {
+        // Define sample so we don't have to repeat it every time
+        $sample = '%'.$keyword.'%';
+
+        // Init where group, so the orWhere statements will be relative only to this group
+        $query->where(function ($query) use ($columns, $sample)
+        {
+            // Search for sample in first column
+            $query->where($columns[0], 'like', $sample);
+
+            // If there's more than one column - search for sample in the rest of them
+            if(count($columns) > 1)
+            {
+                // We're skipping the first column for we already applied it above
+                foreach(array_slice($columns,1) as $column)
+                {
+                    $query->orWhere($column, 'like', $sample);
+                }
+            }
+        });
+
+        return 'Search applied';
+    }
+
+
+    /**
+     * Try to apply findGreater filter and catch any errors
+     * @param Builder $query
+     * @param array $colsAndVals
+     */
+    public static function findGreater(Builder $query, Array $colsAndVals)
+    {
+        // TODO make error handling
+        self::applyFindGreater($query, $colsAndVals);
+    }
+
+    /**
+     * Search for models with values in given columns greater than given values
+     *
+     * @param Builder $query
+     * @param array $colsAndVals
+     */
+    public static function applyFindGreater(Builder $query, Array $colsAndVals)
+    {
+        foreach ($colsAndVals as $col => $val)
+        {
+            if(isset($val))
+            {
+                $query->where($col, '>', $val);
+            }
+        }
+    }
+
+    /**
+     * Try to apply findLesser filter and catch any errors
+     *
+     * @param Builder $query
+     * @param array $colsAndVals
+     */
+    public static function findLesser(Builder $query, Array $colsAndVals)
+    {
+        // TODO make error handling
+        self::applyFindLesser($query, $colsAndVals);
+    }
+
+    /**
+     * Search for models with values in given columns lesser than given values
+     *
+     * @param Builder $query
+     * @param array $colsAndVals
+     */
+    public static function applyFindLesser(Builder $query, Array $colsAndVals)
+    {
+        foreach ($colsAndVals as $col => $val)
+        {
+            if(isset($val))
+            {
+                $query->where($col, '>', $val);
+            }
+        }
+    }
+
+    /**
+     * Try to apply findEqual filter and catch any errors
+     *
+     * @param Builder $query
+     * @param array $colsAndVals
+     */
+    public static function findEqual(Builder $query, Array $colsAndVals)
+    {
+        // TODO make error handling
+        self::applyFindEqual($query, $colsAndVals);
+    }
+
+    /**
+     * Search for models with values in given columns equal to given values
+     *
+     * @param Builder $query
+     * @param array $colsAndVals
+     */
+    public static function applyFindEqual(Builder $query, Array $colsAndVals)
+    {
+        foreach ($colsAndVals as $col => $val)
+        {
+            if(isset($val))
+            {
+                $query->where($col, $val);
+            }
+        }
+    }
+
+    /**
+     * Try to apply findMatchingValues filter and catch any errors
+     *
+     * @param Builder $query
+     * @param array $colsAndVals
+     */
+    public static function findMatchingValues(Builder $query, Array $colsAndVals)
+    {
+        // TODO make error hangling
+        self::applyFindMatchingValues($query, $colsAndVals);
+    }
+
+    /**
+     * Search for model with values in given columns equal to some of given values
+     *
+     * @param Builder $query
+     * @param array $colsAndVals
+     */
+    public static function applyFindMatchingValues(Builder $query, Array $colsAndVals)
+    {
+        foreach($colsAndVals as $col => $val)
+        {
+            $query->whereIn($col, $val);
+        }
+    }
+}
